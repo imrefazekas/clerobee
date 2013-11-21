3d2a73e3da19a390e825dff214681da5272dd7ef
Imre Fazekas <imre.fazekas@icloud.com>
2013-11-21 21:50:17 +0100

    fixes

--- test/CTest.js
+++ test/CTest.js
@@ -7,14 +7,17 @@ var cerobee = new Cerobee( 256 );
 exports.group = {
 
 	testServices: function(test){
-
 		var mID = cerobee.generate();
-		console.log( '\nmID:', mID, mID.length );
+		console.log( '\n mID:', mID, mID.length );
 		var sID = cerobee.generate( mID );
-		console.log( '\nderived:', sID, sID.length );
+		console.log( '\n Derived:', sID, sID.length );
 
-		console.log( '\nisDerived:', cerobee.isDerived( mID, cerobee.generate() ) );
-		console.log( '\nisDerived:', cerobee.isDerived( mID, sID ) );
+		console.log( '\n is new Derived:', cerobee.isDerived( mID, cerobee.generate() ) );
+		console.log( '\n is sID derived:', cerobee.isDerived( mID, sID ) );
+
+
+		var pID = cerobee.generate( { email:'test@provider.org' }, 128 );
+		console.log( '\n pID:', pID, pID.length );
 
 		test.done( );
 	}
